7d45d5379e1529cf3afb9636ffcd484b8f754b11
odix <odix@odix-laptop>
2020-04-15 13:51:50 +0100

    eureka --

--- src/main/java/com/odix/fr/GatewayApplication.java
+++ src/main/java/com/odix/fr/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.odix.fr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,7 +10,7 @@ import com.odix.fr.service.AdministrateurService;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
 	
