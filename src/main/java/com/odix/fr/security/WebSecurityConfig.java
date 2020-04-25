ce1e89c9723168fbc05e43c85ff37f218350bde6
odix <odix@odix-laptop>
2020-04-25 03:12:04 +0100

    httpSecurity.cors().and().csrf().disable()

--- src/main/java/com/odix/fr/security/WebSecurityConfig.java
+++ src/main/java/com/odix/fr/security/WebSecurityConfig.java
@@ -53,8 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity
-				.csrf().disable()
+		httpSecurity.cors().and().csrf().disable()
 				// Don't authenticate this particular request
 				.authorizeRequests()
 				.antMatchers(
