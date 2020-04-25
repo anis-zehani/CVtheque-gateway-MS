d1f9c2d1a1c5062b7d83a4aac75cbdd351cb09ea
odix <odix@odix-laptop>
2020-04-25 03:03:11 +0100

    WebConfig : addCorsMappings

--- src/main/java/com/odix/fr/controller/AuthenticationController.java
+++ src/main/java/com/odix/fr/controller/AuthenticationController.java
@@ -20,7 +20,7 @@ import com.odix.fr.security.JwtTokenUtil;
 import com.odix.fr.security.UserDetailsServiceImpl;
 
 
-@CrossOrigin(origins = "http://35.208.199.180:30000")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/gateway/authentication-controller")
 public class AuthenticationController {
